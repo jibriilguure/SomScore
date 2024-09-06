8827de5b73a3eb1856872d12e9e2c71f0ab4fff6
Jibriil Guure <jibriilguure@Jibriils-MacBook-Air.local>
2024-09-07 00:14:23 +0800

    top scorer fixed when tap new season

--- lib/international/sections/competition/screen/sub-screens/top_scorers_service.dart
+++ lib/international/sections/competition/screen/sub-screens/top_scorers_service.dart
@@ -9,13 +9,15 @@ class TopScorersService {
   static const String apiUrl =
       'https://v3.football.api-sports.io/players/topscorers';
   static const String apiKey = Config.apiFootballApiKey;
+
+  // Fetch top scorers for a given league and season
   Future<List<Player>> fetchTopScorers(int leagueId, String season) async {
     var playerBox = await Hive.openBox<Player>('topScorers');
     var metadataBox =
         await Hive.openBox('metadata'); // Separate box for metadata
 
-    // Check if the cache is expired (data older than 1 day)
-    if (!_isCacheValid(metadataBox)) {
+    // Check if the cache is valid or needs updating
+    if (!_isCacheValid(metadataBox, leagueId, season)) {
       final response = await http.get(
         Uri.parse('$apiUrl?league=$leagueId&season=$season'),
         headers: {
@@ -28,15 +30,17 @@ class TopScorersService {
         final List<Player> players =
             data.map((json) => Player.fromJson(json)).toList();
 
-        // Store new data in Hive and update cache time
+        // Clear previous data in Hive and store the new data
         await playerBox.clear();
         for (var player in players) {
           await playerBox.add(player);
         }
+
+        // Update the cache metadata with the new fetch time and store league/season for validation
         await metadataBox.put(
-            'lastFetchTime',
-            DateTime.now()
-                .toIso8601String()); // Store fetch time in the metadata box
+            'lastFetchTime', DateTime.now().toIso8601String());
+        await metadataBox.put('lastFetchLeague', leagueId);
+        await metadataBox.put('lastFetchSeason', season);
 
         return players;
       } else {
@@ -48,11 +52,20 @@ class TopScorersService {
     }
   }
 
-  // Check if cache is valid (data fetched within last 1 day)
-  bool _isCacheValid(Box metadataBox) {
+  // Validate if the cache is still valid
+  bool _isCacheValid(Box metadataBox, int leagueId, String season) {
     final lastFetchTime = metadataBox.get('lastFetchTime');
-    if (lastFetchTime == null) return false;
+    final lastFetchLeague = metadataBox.get('lastFetchLeague');
+    final lastFetchSeason = metadataBox.get('lastFetchSeason');
 
+    // Cache is invalid if there is no record or the league/season has changed
+    if (lastFetchTime == null ||
+        lastFetchLeague != leagueId ||
+        lastFetchSeason != season) {
+      return false;
+    }
+
+    // Check if the cached data is older than 1 day
     final lastFetchDateTime = DateTime.parse(lastFetchTime);
     return DateTime.now().difference(lastFetchDateTime).inDays < 1;
   }
